b3990ef213c96849e14f4e604045f571742238b8
Rishitha-Gurijala <thisisrishitha@gmail.com>
2024-10-02 22:45:23 +0530

    cdvfdv

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
     <>
+    <h1 className='text-danger'>E-Commerce website in React js</h1>
     </>
   );
 }
