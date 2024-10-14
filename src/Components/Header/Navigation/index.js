14faf8e3d9b536f18afb72ae53444ad07ac9ee0b
Rishitha-Gurijala <thisisrishitha@gmail.com>
2024-10-14 19:31:53 +0530

    shcbsd

--- src/Components/Header/Navigation/index.js
+++ src/Components/Header/Navigation/index.js
@@ -2,20 +2,39 @@ import Button from '@mui/material/Button';
 import { IoIosMenu } from "react-icons/io";
 import { FaAngleDown } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Navigation = () => {
-    return (
+    const [isopenSidebarVal, setisopenSidebarVal] = useState(true);
 
+    return (
         <nav>
             <div className="container">
                 <div className="row">
                     <div className="col-sm-2 navPart1">
                         <div className='catWrapper'>
-                            <Button className="allCatTab align-items-center">
+                            <Button className="allCatTab align-items-center" onClick={()=>setisopenSidebarVal(!isopenSidebarVal)}>
                                 <span className="icon1 mr-2"><IoIosMenu /></span>
                                 <span class="text">ALL CATEGORIES</span>
                                 <span className="icon2 ml-2"><FaAngleDown /></span>
                             </Button>
+
+                            <div className={`sidebarNav ${isopenSidebarVal === true ? 'open' : ''}`}>
+                                <ul>
+                                    <li><Link to="/"><Button>men</Button></Link></li>
+                                    <li><Link to="/"><Button>women</Button></Link></li>
+                                    <li><Link to="/"><Button>beauty</Button></Link></li>
+                                    <li><Link to="/"><Button>watches</Button></Link></li>
+                                    <li><Link to="/"><Button>kids</Button></Link></li>
+                                    <li><Link to="/"><Button>gift</Button></Link></li>
+                                    <li><Link to="/"><Button>men</Button></Link></li>
+                                    <li><Link to="/"><Button>women</Button></Link></li>
+                                    <li><Link to="/"><Button>beauty</Button></Link></li>
+                                    <li><Link to="/"><Button>watches</Button></Link></li>
+                                    <li><Link to="/"><Button>kids</Button></Link></li>
+                                    <li><Link to="/"><Button>gift</Button></Link></li>
+                                </ul>
+                            </div>
                         </div>
 
                     </div>
@@ -25,6 +44,9 @@ const Navigation = () => {
                             <li className='list-inline-item'>
                                 <Link to="/"><Button>men</Button></Link>
                                 <div className='submenu shadow'>
+                                    <Link to="/"><Button>clothing</Button></Link>
+                                    <Link to="/"><Button>footwear</Button></Link>
+                                    <Link to="/"><Button>watches</Button></Link>
                                     <Link to="/"><Button>clothing</Button></Link>
                                     <Link to="/"><Button>footwear</Button></Link>
                                     <Link to="/"><Button>watches</Button></Link>
